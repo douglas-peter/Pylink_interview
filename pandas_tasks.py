fb0c0792ec6d2e2e364ce66067d9bb7d2593b244
douglas-peter <76939369+douglas-peter@users.noreply.github.com>
2021-07-12 09:34:27 +0200

    update

---
+++ pandas_tasks.py
@@ -0,0 +1,11 @@
+
+if __name__ == "__main__":
+
+    # 1) read csv, hint dates in CSVs are read as strings, please convert to datetime N.B. dates are stored in UK format
+    # i.e. %d/%m/%Y
+
+    # 2) Create a new dataframe from the original that shows the total sales in a month for each given customer, fill
+    # NAs with zeroes and print the new dataframe
+
+    # 3) Print the series containing the customer information from the original df where the maximum single sale occurs
+
