72080a10e17418dabab9c7eacaf63591ef1701a7
douglas-peter <76939369+douglas-peter@users.noreply.github.com>
2021-07-12 08:36:23 +0200

    Initial commit

---
+++ fizz_buzz.py
@@ -0,0 +1,5 @@
+# The aim of this section is to write a function that gives to correct solution to the children's counting game
+# FizzBuzz up until the user defined upper bound and then print the result. If you are not familiar with this game,
+# it works as so. You start counting from one but when you reach a multiple of three you replace the number with the
+# word "Fizz" and when you reach a multiple of five, you replace the number with "Buzz", if the number is a multiple of
+# three and five (e.g. 15) then it is replace by the combined "FizzBuzz".
